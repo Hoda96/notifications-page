0fdcef5a1ca747b10d0ecaf893107296a18b6cdb
Hoda96 <farahani.h40@gmail.com>
2023-09-08 17:48:26 +0330

    Js file Update#1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@
 
 const markAllRead = document.querySelector(".all-read");
 const notificationNumber = document.querySelector(".notification-number");
+const unreadNotificationContainer = document.querySelectorAll(
+  ".unread-notification-container"
+);
 
-console.log(notificationNumber.textContent);
+let notifiNum = notificationNumber.textContent;
 
-markAllRead.addEventListener("click", test);
+// Mark all read
+markAllRead.addEventListener("click", function () {
+  unreadNotificationContainer.forEach((container) => {
+    container.style.backgroundColor = "#FFF";
+    notificationNumber.textContent = 0;
+  });
+});
 
-function test() {
-  alert("all read");
-}
+// Read each notification
+let seen = true;
+
+unreadNotificationContainer.forEach((container) => {
+  container.addEventListener("click", () => {
+    container.style.backgroundColor = "#fff";
+
+    // container.classList.remove("bg-VeryLightGrayishBlue");
+    // container.classList.add("bg-white");
+
+    notificationNumber.textContent = notificationNumber.textContent - 1;
+
+    // if (notificationNumber < 0) return;
+  });
+});
